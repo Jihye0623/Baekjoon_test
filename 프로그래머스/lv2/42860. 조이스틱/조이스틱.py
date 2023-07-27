5f8d53aacd31304b1835f66a6876e6e7c581a1ae
Jihye0623 <123048615+Jihye0623@users.noreply.github.com>
2023-07-27 17:35:00 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -2,11 +2,15 @@ def solution(name):
 
     answer = 0
     
+    # 기본적인 최소 움직임
     min_move = len(name) - 1
     
+    
     for i, char in enumerate(name):
+        # A부터 오름차순으로 찾는 것이 빠른지, 아니면 Z부터 내림차순으로 찾는 것이 빠른지 비교
         answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
         
+        # 연속된 A 찾기
         next = i + 1
         while next < len(name) and name[next] == 'A':
             next += 1
