1c6da1ee9e2a5e8ea1f060d575889b79dcd3bd52
Jihye <123048615+Jihye0623@users.noreply.github.com>
2024-09-03 09:50:39 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+int n, k;
+cin >> n >> k;
+queue<int> q;
+for (int i = 1; i <= n; i++) {
+q.push(i);
+}
+cout << "<";
+
+while (q.size() > 1) {
+int cnt = k;
+for (int i = 0; i < k-1; i++) {
+q.push(q.front());
+q.pop();
+}
+cout << q.front() << ", ";
+q.pop();
+}
+cout << q.front() << ">";
+}
