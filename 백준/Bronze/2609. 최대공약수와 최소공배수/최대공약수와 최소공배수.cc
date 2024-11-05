19ecefedc7e13be7f687175879328014f498d392
Jihye <123048615+Jihye0623@users.noreply.github.com>
2024-11-05 22:12:02 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+int gcd(int x, int y) {
+if (y == 0) return x;
+else return gcd(y, x % y);
+}
+int main() {
+int a, b;
+scanf("%d %d", &a, &b);
+int g = gcd(a,b);
+printf("%d\n%d", g, a* b / g);
+return 0;
+}
