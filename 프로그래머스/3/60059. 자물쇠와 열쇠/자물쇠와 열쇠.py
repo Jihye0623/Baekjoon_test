dfd12f3b3f3c44408e6dc654da34be91cd670ab6
Jihye <123048615+Jihye0623@users.noreply.github.com>
2025-09-29 16:18:19 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 652.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,45 +1,37 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/60059
-
-def rotate_a_matrix_by_90_degree(a):
-    n = len(a)
-    m = len(a[0])
-    result = [[0]*n for _ in range(m)]
-    for i in range(n):
+def turn_right(key, m):
+    temp = [[0]*m for _ in range(m)]
+    for i in range(m):
         for j in range(m):
-            result[j][n-i-1] = a[i][j]
-    return result
+            temp[i][j] = key[(m-j-1)%m][i]
+    return temp
 
-def check(new_lock):
-    lock_length = len(new_lock) // 3
-    for i in range(lock_length, lock_length*2):
-        for j in range(lock_length, lock_length*2):
-            if new_lock[i][j] != 1:
+def check(nlock, n):
+    for i in range(n,n*2):
+        for j in range(n,n*2):
+            if nlock[i][j] != 1:
                 return False
     return True
-
+    
 def solution(key, lock):
-    n = len(lock)
-    m = len(key)
-    # 자물쇠의 크기를 기존의 3배로 변환
-    new_lock = [[0]*(n*3) for _ in range(n*3)]
-    # 새 자물쇠 중앙에 기존 자물쇠 넣기
-    for i in range(n):
-        for j in range(n):
-            new_lock[i+n][j+n] = lock[i][j]
-            
-    # 4가지 방향에 대해 확인
-    for ratation in range(4):
-        key = rotate_a_matrix_by_90_degree(key)
+    answer = False
+    n,m = len(lock), len(key)
+    graph = [[0]*n*3 for _ in range(n*3)]
+    for i in range(n,2*n):
+        for j in range(n,2*n):
+            graph[i][j] = lock[i%n][j%n]
+
+    for r in range(4):
+        key = turn_right(key,m)
         for x in range(n*2):
             for y in range(n*2):
-                # 자물쇠에 열쇠 끼워 넣기
                 for i in range(m):
                     for j in range(m):
-                        new_lock[x+i][y+j] += key[i][j]
-                if check(new_lock) == True:
+                        graph[x+i][y+j] += key[i][j]
+
+                if check(graph,n):
                     return True
-                # 자물쇠에 열쇠 다시 빼기
                 for i in range(m):
                     for j in range(m):
-                        new_lock[x+i][y+j] -= key[i][j]
-    return False
+                        graph[x+i][y+j] -= key[i][j]
+        
+    return answer
