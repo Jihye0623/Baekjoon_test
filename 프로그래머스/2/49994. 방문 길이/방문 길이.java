3b2033760a4753b87665154bf0da93cc2dd6fc9b
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-01-12 22:23:25 +0900

    [level 2] Title: 방문 길이, Time: 16.56 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,37 +1,37 @@
-import java.util.HashMap;
-import java.util.HashSet;
-
+import java.util.*;
 
 class Solution {
-    private static boolean isValidMove(int nx, int ny){
-        return 0<=nx && nx<11 && 0<=ny && ny<11;    
+    
+    private static HashMap<Character, int[]> direction;
+    
+    private static void initDirection(){
+        direction.put('U', new int[]{0,-1});
+        direction.put('D', new int[]{0,1});
+        direction.put('L', new int[]{-1,0});
+        direction.put('R', new int[]{1,0});
     }
     
-    private static final HashMap<Character, int[]> location = new HashMap<>();
-    
-    private static void initLocation(){
-        location.put('U', new int[]{0,1});
-        location.put('D', new int[]{0,-1});
-        location.put('L', new int[]{-1,0});
-        location.put('R', new int[]{1,0});
+    private static boolean isValid(int x, int y){
+        return 0<=x && x<11 && 0<=y && y<11;
     }
     
     public int solution(String dirs) {
-        initLocation();
+        direction = new HashMap<>();
+        initDirection();
         int x=5, y=5;
-        HashSet<String> answer = new HashSet();
         
-        for(int i=0; i<dirs.length(); i++){
-            int[] offset = location.get(dirs.charAt(i));
-            int nx = x + offset[0];
-            int ny = y + offset[1];
-            if(!isValidMove(nx,ny))
-                continue;
-            answer.add(x+" "+y+" "+nx+" "+ny);
-            answer.add(nx+" "+ny+" "+x+" "+y);
-            x=nx;
-            y=ny;
+        HashSet<String> way = new HashSet<>();
+        for(int i = 0; i<dirs.length(); i++){
+            int[] offset = direction.get(dirs.charAt(i));
+            int dx = x + offset[0];
+            int dy = y + offset[1];
+            if(isValid(dx,dy)){
+                way.add(x+" "+y+" " + dx+" "+dy);
+                way.add(dx+" "+dy+" " + x+" "+y);
+                x = dx;
+                y = dy;
+            }
         }
-        return answer.size()/2;
+        return way.size()/2;
     }
 }
