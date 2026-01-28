8ef2222e507275a7ff0506cdc6e94cd0fbdf65ed
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-01-29 00:07:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 183.03 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, HashSet<String>> reportedUser = new HashMap<>();
+        HashMap<String, HashSet<String>> map = new HashMap<>();
         HashMap<String, Integer> count = new HashMap<>();
         
-        for(String r:report){
-            String[] s = r.split(" ");
-            String userId = s[0];
-            String reportedId = s[1];
-            
-            if(!reportedUser.containsKey(reportedId)){
-                reportedUser.put(reportedId, new HashSet<>());
+        for(String rep:report){
+            String[] re = rep.split(" ");
+            String userId = re[0];
+            String reportedId = re[1];
+            if(!map.containsKey(reportedId)){
+                map.put(reportedId, new HashSet<>());
             }
-            
-            reportedUser.get(reportedId).add(userId);
+            map.get(reportedId).add(userId);
         }
         
-        for(Map.Entry<String, HashSet<String>> entry: reportedUser.entrySet()){
+        for(Map.Entry<String, HashSet<String>> entry : map.entrySet()){
             if(entry.getValue().size()>=k){
-                for(String uid:entry.getValue()){
-                    count.put(uid, count.getOrDefault(uid, 0)+1);
-                }
+                for(String uid:entry.getValue())
+                    count.put(uid, count.getOrDefault(uid,0)+1);
             }
         }
         
-        int[] answer = new int[id_list.length];
-        
-        for(int i =0; i<id_list.length; i++){
-            answer[i] = count.getOrDefault(id_list[i],0);
+        int[] result = new int[id_list.length];
+        for(int i = 0; i<id_list.length; i++){
+            result[i] = count.getOrDefault(id_list[i], 0);
         }
         
-        return answer;
+        return result;
     }
 }
