a15e996439dce935f73943a7ea4aeb0eb74806ff
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-01 13:01:36 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 28.65 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -3,42 +3,50 @@ import java.util.*;
 class Solution {
     private static HashMap<Integer, HashMap<String, Integer>> courseMap;
     
-    public String[] solution(String[] orders, int[] course) {
-        courseMap = new HashMap<>();
-        for(int i:course){
-            courseMap.put(i, new HashMap<>());
-        }
-        
-        for(String order:orders){
-            char[] orderArray = order.toCharArray();
-            Arrays.sort(orderArray);
-            combinations(0,orderArray,"");
-        }
-        
-        ArrayList<String> answer = new ArrayList<>();
-        
-        for(HashMap<String, Integer> count: courseMap.values()){
-            count.values()
-                .stream()
-                .max(Comparator.comparingInt(o->o))
-                .ifPresent(cnt->count.entrySet()
-                          .stream()
-                          .filter(entry->cnt.equals(entry.getValue())&&cnt>1)
-                          .forEach(entry->answer.add(entry.getKey())));
-        }
-        Collections.sort(answer);
-        return answer.toArray(new String[0]);
-    }
-    
-    
-    public static void combinations(int idx, char[]order, String result){
+    private static void combination(int idx, char[] order, String result){
         if(courseMap.containsKey(result.length())){
             HashMap<String, Integer> map = courseMap.get(result.length());
             map.put(result, map.getOrDefault(result, 0)+1);
         }
         
-        for(int i=idx; i<order.length; i++){
-            combinations(i+1, order, result+order[i]);
+        for(int i = idx; i<order.length; i++){
+            combination(i+1, order, result+order[i]);
         }
+    }
+    public String[] solution(String[] orders, int[] course) {
+        courseMap = new HashMap<>();
+        for(int c:course){
+            courseMap.put(c, new HashMap<>());
+        }
+        
+        for(String order:orders){
+            char[] orderArray = order.toCharArray();
+            Arrays.sort(orderArray);
+            combination(0, orderArray,"");
+        }
+        
+        ArrayList<String> result = new ArrayList<>();
+        
+        for(HashMap<String, Integer> count : courseMap.values()){
+            int maxMenu = 0;
+            for(Map.Entry<String, Integer> entry: count.entrySet()){
+                if(entry.getValue()>=2)
+                    if(maxMenu<entry.getValue()) 
+                        maxMenu = entry.getValue();
+            }
+            
+            for(Map.Entry<String, Integer> entry: count.entrySet()){
+                if(entry.getValue()==maxMenu)
+                    result.add(entry.getKey());
+            }
+        }
+        
+        Collections.sort(result);
+        String[] answer = new String[result.size()];
+        for(int i = 0; i<result.size(); i++){
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
     }
 }
