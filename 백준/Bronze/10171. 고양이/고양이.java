b5c34283688e8b6bf4caeeae6660575c1e987288
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-05 17:23:51 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append("\n");
+        sb.append(" )  ( ')").append("\n");
+        sb.append("(  /  )").append("\n");
+        sb.append(" \\(__)|");
+        
+        System.out.println(sb.toString());
+    }
+}
