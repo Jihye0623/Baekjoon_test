bdce3f64a577d864981b9b5d000005cc93ccd64b
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-05 21:17:48 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -4,16 +4,13 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
-        
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 1; i<10; i++){
-            sb.append(n + " * "+ i +" = " +n*i +"\n");
+        for(int i = 1; i<=9; i++){
+            sb.append(n).append(" * ").append(i).append(" = ").append(n*i).append("\n");
         }
         
         System.out.println(sb.toString());
-        
     }
 }
