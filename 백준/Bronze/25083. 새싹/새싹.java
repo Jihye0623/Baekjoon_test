f154537a66f6edb3490166217a41542c34d22916
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-06 16:27:21 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+       StringBuilder sb = new StringBuilder();
+       sb.append("         ,r\'\"7").append("\n");
+       sb.append("r`-_   ,'  ,/").append("\n");
+       sb.append(" \\. \". L_r'").append("\n");
+       sb.append("   `~\\/").append("\n");
+       sb.append("      |").append("\n");
+       sb.append("      |").append("\n");
+        
+       System.out.println(sb.toString());
+        
+    }
+}
