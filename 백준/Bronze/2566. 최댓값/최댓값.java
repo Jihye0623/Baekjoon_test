10e06e02e65226707f1f8215f7ca10af6c4b6be7
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-06 16:03:27 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -5,26 +5,23 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int[][] arr = new int[10][10];
+        int max = -1, mi = -1, mj = -1;
         
         for(int i = 1; i<10; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j<10; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int max = -1, ii=0, jj=0;
-        for(int i = 1; i<10; i++){
-            for(int j = 1; j<10; j++){
-                if (arr[i][j]>max){
-                    max = arr[i][j];
-                    ii = i;
-                    jj = j;
+            for(int j=1;j<10;j++){
+                int num = Integer.parseInt(st.nextToken());
+                if(max<num){
+                    max = num;
+                    mi = i;
+                    mj = j;
                 }
             }
         }
         
-        System.out.println(arr[ii][jj]);
-        System.out.printf("%d %d", ii, jj);
+		StringBuilder sb = new StringBuilder();
+        sb.append(max).append("\n").append(mi).append(" ").append(mj);
+        
+        System.out.println(sb.toString());
     }
 }
