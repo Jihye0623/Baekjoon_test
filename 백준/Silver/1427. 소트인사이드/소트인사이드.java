6cc0ac8dd194febed8d8586dca022a69c8110dbc
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-12 16:59:19 +0900

    [Silver V] Title: 소트인사이드, Time: 116 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str  = br.readLine();
+        int[] arr = new int[str.length()];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = (int) (str.charAt(i)-'0');
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1; i>=0; i--) sb.append(arr[i]);
+        
+        System.out.println(sb.toString());
+    }
+}
