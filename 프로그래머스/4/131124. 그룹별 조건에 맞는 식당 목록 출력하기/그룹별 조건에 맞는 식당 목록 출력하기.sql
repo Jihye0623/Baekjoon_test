6fa1981ed14891d4937efa923c0a870a0322400e
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-16 00:11:50 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+-- 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL문을 작성해주세요. 
+-- 회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력되도록 작성해주시고, 
+-- 결과는 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요.
+SELECT M.MEMBER_NAME, R.REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE,"%Y-%m-%d")
+FROM MEMBER_PROFILE M JOIN REST_REVIEW R ON M.MEMBER_ID = R.MEMBER_ID
+WHERE M.MEMBER_ID IN (SELECT MEMBER_ID
+                     FROM REST_REVIEW
+                     GROUP BY MEMBER_ID
+                      HAVING COUNT(*) = (
+                      SELECT COUNT(*)
+                          FROM REST_REVIEW 
+                          GROUP BY MEMBER_ID
+                          ORDER BY COUNT(*) DESC
+                          LIMIT 1 
+                      )
+                     
+                     )
+ORDER BY 3, 2
