1f988183515687b7989e687c113921ab0390b812
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-02-19 11:20:45 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 24152 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    private static int[] dp;
+    private static int[] arr;
+    
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        arr = new int[n];
+        dp = new int[n];
+           
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+        }
+        
+        int answer = Arrays.stream(dp).max().getAsInt();
+        System.out.println(answer);
+        
+        
+        
+    }
+}
