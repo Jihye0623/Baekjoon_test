235bbf64a43d2d6d8fc1401084af87ed27605f4d
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 08:28:03 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,22 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int[] money = {25, 10, 5, 1};
-        StringBuilder sb = new StringBuilder();
+        int[] money = {25,10,5,1};
         
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i<T; i++){
             int c = Integer.parseInt(br.readLine());
-            for(int m : money){
-                sb.append(c/m).append(" ");
-                c %= m;
+            int j = 0;
+            int[] temp = new int[4];
+            while(c>0){
+                temp[j] = c/money[j];
+                c %= money[j];
+                j++;
             }
+            for(int t: temp) sb.append(t).append(" ");
             sb.append("\n");
         }
-        System.out.println(sb.toString());
         
+        System.out.println(sb);
     }
 }
