392629e56da0498e8dd26cdc31743e32d09308ab
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 16:57:15 +0900

    [Silver V] Title: 나이순 정렬, Time: 660 ms, Memory: 51088 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -5,30 +5,33 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<String[]> list = new ArrayList<>();
-		StringBuilder sb = new StringBuilder();
-        
-        StringTokenizer st;
+             
+        ArrayList<String[]> arr = new ArrayList<>();
         for(int i = 0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st=  new StringTokenizer(br.readLine());
             String age = st.nextToken();
             String name = st.nextToken();
-            list.add(new String[]{age, name});
+            
+            arr.add(new String[]{age, name});
         }
         
-        Collections.sort(list, new Comparator<String[]>(){
-           @Override
-            public int compare(String[]o1, String[]o2){
-                int age1 = Integer.parseInt(o1[0]);
-                int age2 = Integer.parseInt(o2[0]);
-                return Integer.compare(age1, age2);
+        Collections.sort(arr, new Comparator<String[]>(){
+            @Override
+            public int compare(String[] op1, String[] op2){
+                int o1 = Integer.parseInt(op1[0]);
+                int o2 = Integer.parseInt(op2[0]);
+                
+                int result = Integer.compare(o1,o2);
+                return result;
             }
         });
         
-        for(String[] str : list){
-            sb.append(str[0]).append(" ").append(str[1]).append("\n");
+        StringBuilder sb = new StringBuilder();
+        for(String[] op: arr){
+            sb.append(op[0]).append(" ").append(op[1]).append("\n");
         }
         
-        System.out.println(sb.toString());
+        System.out.println(sb);
+    
     }
 }
