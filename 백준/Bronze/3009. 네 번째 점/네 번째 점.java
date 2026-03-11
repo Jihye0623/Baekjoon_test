97b6af1fa791858c90a9c17d0f042065476382d6
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 10:36:39 +0900

    [Bronze III] Title: 네 번째 점, Time: 116 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -4,22 +4,23 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         HashSet<Integer> setX = new HashSet<>();
         HashSet<Integer> setY = new HashSet<>();
-        
-		for(int i = 0; i<3; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            if (setX.contains(a)) setX.remove(a);
-            else setX.add(a);
-            if (setY.contains(b)) setY.remove(b);
-            else setY.add(b);
-        }
 
+        for(int i = 0; i<3; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if(setX.contains(x)) setX.remove(x);
+            else setX.add(x);
+            
+            if(setY.contains(y)) setY.remove(y);
+            else setY.add(y);
+        }
+        
         for(int i:setX) System.out.print(i + " ");
         for(int i:setY) System.out.println(i);
+        
         
     }
 }
