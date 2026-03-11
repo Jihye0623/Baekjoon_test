ee8aed1e44540219c720037553ea1bc8d2b9c075
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 10:52:27 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -4,27 +4,24 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        
         while(true){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
             if(a==0 && b==0 && c==0) break;
-                
             int max = Math.max(a, Math.max(b,c));
-
-            if (max>=(a+b+c-max)) sb.append("Invalid");
-            else if(a==b&&b==c) sb.append("Equilateral");
-            else if((a==b && b!=c) || (a!=b && b==c) || (a!=b && a==c)) sb.append("Isosceles");
-            else sb.append("Scalene");
-            
-            
+                
+            if(a==b && b==c) sb.append("Equilateral");
+            else if(max>=(a+b+c-max)) sb.append("Invalid");
+            else if (a!=b && b!=c && a!=c) sb.append("Scalene");
+            else sb.append("Isosceles");
             sb.append("\n");
         }
         
-        System.out.println(sb.toString());
+        System.out.println(sb);
+		    
     }
 }
