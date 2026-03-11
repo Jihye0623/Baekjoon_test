d72cd80eb85259fdf422608a4ed56525f544a9ac
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 09:04:13 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -4,20 +4,23 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine())-1;
-        if(n==0){
+        int n = Integer.parseInt(br.readLine());
+        
+        long answer = 1;
+        long number = 1;
+
+        if(n==1) {
             System.out.println(1);
             return;
         }
         
-        int answer=1;
-        int total = 6;
         while(true){
-            if(total>=n) break;
-            answer++;
-            total = 6*answer + total;
+            if(number >=  n) break;
+            number += 6* answer;
+            answer += 1;
+            
         }
-        System.out.println(answer+1);
-
+        
+        System.out.println(answer);
     }
 }
