9c7e807780eaca77de152ef58cf18cc0725445cc
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 09:35:43 +0900

    [Silver V] Title: 분수찾기, Time: 136 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,37 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int num = 0;
-        int idx = 1;
-        while (num<n){
-            num += idx++;
+        int x = Integer.parseInt(br.readLine());
+        
+        int row = 1, col = 1;
+        int temp = 1;
+        boolean flag = true; 
+        while(true){
+            if(temp==x) break;
+            temp += 1;
+            if(flag){
+                if(row==1){
+                    col++;
+                    flag = !flag;
+                }
+                else{
+                    row--;
+                    col++;
+                }
+            }
+            else{
+                if(col==1){
+                    row++;
+                    flag = !flag;
+                }
+                else{
+                    row++;
+                    col--;
+                }
+                
+            }
         }
-        idx--;
-        n = n - num + idx;
         
         StringBuilder sb = new StringBuilder();
-        if(idx%2==0){
-            int x = 1, y = idx;
-            for(int i = 1; i<n; i++){
-                x++;
-                y--;
-            }
-            sb.append(x).append("/").append(y);
-        }
-        else{
-            int x = idx, y = 1;
-            for(int i = 1; i<n; i++){
-                x--;
-                y++;
-            }
-            sb.append(x).append("/").append(y);
-        }
-
-        System.out.println(sb.toString());
+        sb.append(row).append("/").append(col);
+        System.out.println(sb);
     }
 }
