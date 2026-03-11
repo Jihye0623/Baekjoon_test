dc6aa3d251547049a014a92258d3cc57f499b053
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 16:52:44 +0900

    [Silver V] Title: 단어 정렬, Time: 1412 ms, Memory: 26156 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -5,36 +5,33 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        HashSet<String> set = new HashSet<>();
         
-        for(int i=0; i<n; i++){
-            set.add(br.readLine());
+        ArrayList<String> arr = new ArrayList<>();
+        
+        for(int i = 0; i<n; i++){
+            String str = br.readLine();
+            if(arr.contains(str)) continue;
+            arr.add(str);
         }
-
-        ArrayList<String> list = new ArrayList<>();
-        for(String str:set){
-            list.add(str);
-        }
-
-        Collections.sort(list, new Comparator<String>(){
-           @Override
-            public int compare(String o1, String o2){
-                int len1 = o1.length();
-                int len2 = o2.length();
-
-                int result = Integer.compare(len1,len2);
-                if(result == 0) return o1.compareTo(o2);
-                return result;
+        
+        Collections.sort(arr, new Comparator<String>(){
+            @Override
+            public int compare(String op1, String op2){
+                int o1 = op1.length();
+                int o2 = op2.length();
+                
+                int result = Integer.compare(o1,o2);
+                if(result == 0) return op1.compareTo(op2);
+                else return result;
             }
         });
-
+        
         StringBuilder sb = new StringBuilder();
-        
-        for(String str:list){
-            sb.append(str).append("\n");
+        for(String op: arr){
+            sb.append(op).append("\n");
         }
-
         
-        System.out.println(sb.toString());
+        System.out.println(sb);
+    
     }
 }
