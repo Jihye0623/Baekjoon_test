f51db9acf7033f2c015120719668d43378f8aadf
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-11 16:45:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 616 ms, Memory: 49796 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -5,21 +5,36 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<int[]> list = new ArrayList<>();
-        StringTokenizer st;
+        
+        ArrayList<int[]> arr = new ArrayList<>();
+        
         for(int i = 0; i<n; i++){
-            st = new StringTokenizer(br.readLine());    
-            int x = Integer.parseInt(st.nextToken()); 
-            int y = Integer.parseInt(st.nextToken());
-            list.add(new int[]{x,y});
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] temp = new int[2];
+            temp[0] = Integer.parseInt(st.nextToken());
+            temp[1] = Integer.parseInt(st.nextToken());
+            
+            arr.add(temp);
         }
+        
+        Collections.sort(arr, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] op1, int[] op2){
+                int x1 = op1[0];
+                int x2 = op2[0];
+                
+                int result = Integer.compare(x1,x2);
+                if(result == 0) return Integer.compare(op1[1], op2[1]);
+                else return result;
+            }
+        });
         
         StringBuilder sb = new StringBuilder();
-        Collections.sort(list, (o1,o2)-> Integer.compare(o1[0],o2[0])==0?Integer.compare(o1[1],o2[1]):Integer.compare(o1[0],o2[0]));
-        for(int[] arr : list){
-            sb.append(arr[0]).append(" ").append(arr[1]).append("\n");
+        for(int[] op: arr){
+            sb.append(op[0]).append(" ").append(op[1]).append("\n");
         }
         
-        System.out.println(sb.toString());
+        System.out.println(sb);
+    
     }
 }
