7cf7972fa274dd34ecccd28e5b54df052b7590c7
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-12 09:55:46 +0900

    [Silver IV] Title: 가로수, Time: 216 ms, Memory: 23216 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -6,12 +6,14 @@ class Main{
         if(b==0) return a;
         else return gcd(b,a%b);
     }
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        
         int[] tree = new int[n];
         for(int i = 0; i<n; i++){
-            tree[i] = Integer.parseInt(br.readLine());
+           tree[i] = Integer.parseInt(br.readLine());
         }
         
         int[] distance = new int[n-1];
@@ -24,12 +26,11 @@ class Main{
             g = gcd(g, distance[i]);
         }
         
-        int cnt = 0;
+        int answer = 0;
         for(int i = 0; i<n-1; i++){
-            cnt += distance[i]/g -1;
+            answer += distance[i]/g - 1;
         }
         
-        System.out.println(cnt);
-        
+        System.out.println(answer);
     }
 }
