d893807ad27d649a3138d61d9ffbb29c551e4c2f
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-13 08:39:34 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.java
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -5,11 +5,7 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-		int answer = 1;
-        for(int i = 1; i<=n; i++){
-            answer *= 2;
-        }
         
-        System.out.println(answer);
+        System.out.println((int)Math.pow(2,n));
     }
 }
