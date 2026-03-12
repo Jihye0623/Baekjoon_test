fbe8141f72dc0fc49cff3d568e871b68c24c170c
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-13 08:41:45 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -5,6 +5,7 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        
         int answer = 1;
         for(int i = 2; i<=n; i++){
             answer *= i;
