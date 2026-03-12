ed8d7b01b77dd737f7292718ebb6e3b5e56b72f1
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-12 09:24:09 +0900

    [Silver IV] Title: 듣보잡, Time: 248 ms, Memory: 26220 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -5,30 +5,23 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        HashSet<String> set1 = new HashSet<>();
-        HashSet<String> set2 = new HashSet<>();
-        for(int i = 0; i<n; i++){
-            set1.add(br.readLine());
-        }
-        for(int i = 0; i<m; i++){
-            set2.add(br.readLine());
+        HashSet<String> s1 = new HashSet<>();
+		ArrayList<String> arr = new ArrayList<>();
+        
+        for(int i = 0; i<n; i++) s1.add(br.readLine());
+        for(int i = 0; i<m; i++) {
+            String s = br.readLine();
+            if(s1.contains(s)) arr.add(s);
         }
         
-        int cnt = 0;
-        ArrayList<String> list = new ArrayList<>();
-        for(String str:set1){
-            if(set2.contains(str)){
-                cnt++;
-                list.add(str);
-            }
-        }
+        Collections.sort(arr);
+        
         StringBuilder sb = new StringBuilder();
-        Collections.sort(list);
-        for(String str:list) sb.append(str).append("\n");
+        for(String s:arr) sb.append(s).append("\n");
         
-        System.out.println(cnt);
-        System.out.println(sb.toString());
+        System.out.println(arr.size());
+        System.out.println(sb);
     }
 }
