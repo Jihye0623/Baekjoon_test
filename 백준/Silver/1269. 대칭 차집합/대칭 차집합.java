5a2dab76ec750e8273a6679c0ffce4242258e2d2
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-12 09:29:10 +0900

    [Silver IV] Title: 대칭 차집합, Time: 600 ms, Memory: 67008 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -5,27 +5,24 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        HashSet<Integer> setA = new HashSet<>();
-        HashSet<Integer> setB = new HashSet<>();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        HashSet<Integer> s1 = new HashSet<>();
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<a; i++){
-            setA.add(Integer.parseInt(st.nextToken()));
+        for(int i = 0; i<n; i++){
+            s1.add(Integer.parseInt(st.nextToken()));
         }
-        
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<b; i++){
-            setB.add(Integer.parseInt(st.nextToken()));
+        int minus = 0;
+        for(int i = 0; i<m; i++) {
+            int k = Integer.parseInt(st.nextToken());
+            if(s1.contains(k)) minus++;
         }
+
+        System.out.println(n+m - 2*minus);
         
-        HashSet<Integer> set = new HashSet<>();
-        for(int k:setA){
-            if(!setB.contains(k)) set.add(k);
-        }
-        for(int k:setB){
-            if(!setA.contains(k)) set.add(k);
-        }
-        System.out.println(set.size());
+        
     }
 }
