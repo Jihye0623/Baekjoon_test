58ba6bb8f45fbf956621fc0a3aa82e59e64e0333
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-12 09:05:07 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 652 ms, Memory: 49336 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -5,25 +5,22 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        
+    
         HashSet<String> set = new HashSet<>();
         for(int i = 0; i<n; i++){
-            st = new StringTokenizer(br.readLine());  
+            StringTokenizer st = new StringTokenizer(br.readLine());   
             String name = st.nextToken();
-            String status = st.nextToken();
-            if(status.startsWith("e")) set.add(name);
+            String check = st.nextToken();
+            
+            if(check.startsWith("e")) set.add(name);
             else set.remove(name);
         }
         
-        ArrayList<String> list = new ArrayList<>();
-        for(String str:set) list.add(str);
-        Collections.sort(list, Collections.reverseOrder());
-        
+        ArrayList<String> answer = new ArrayList<>(set);
+        Collections.sort(answer, Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
-        for(String str:list) sb.append(str).append("\n");
+        for(String s : answer) sb.append(s).append("\n");
         
-        System.out.println(sb.toString());
-		    
+        System.out.println(sb);
     }
 }
