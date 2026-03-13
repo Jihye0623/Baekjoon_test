a9be1ada6c1fe7b9d185f4ffe20fd53f91ccc752
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-13 09:36:06 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 740 ms, Memory: 37960 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -6,36 +6,36 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        ArrayList<String> words = new ArrayList<>();
+		int m = Integer.parseInt(st.nextToken());
+        
         HashMap<String, Integer> map = new HashMap<>();
+        
         for(int i = 0; i<n; i++){
             String word = br.readLine();
-            if(word.length()<m) continue;
-            map.put(word, map.getOrDefault(word, 0)+1);
+            if(word.length() < m) continue;
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
         
         ArrayList<String> keyList = new ArrayList<>(map.keySet());
         Collections.sort(keyList, new Comparator<String>(){
            @Override
-            public int compare(String s1, String s2){
-                int cnt1 = map.get(s1);
-                int cnt2 = map.get(s2);
-                int result1 = Integer.compare(cnt2, cnt1);
-                if(result1!=0) return result1;
-                
-                int len1 = s1.length();
-                int len2 = s2.length();
-                int result2 = Integer.compare(len2, len1);
-                if(result2!=0) return result2;
-                
-                return s1.compareTo(s2);
-            }
+           public int compare(String k1, String k2){
+               int v1 = map.get(k1);
+               int v2 = map.get(k2);
+               int result = Integer.compare(v2,v1);
+               if(result != 0) return result;
+               
+               int len1 = k1.length();
+               int len2 = k2.length();
+               int result2 = Integer.compare(len2, len1);
+               if(result2!=0) return result2;
+           
+               return k1.compareTo(k2);
+           }
         });
         
         StringBuilder sb = new StringBuilder();
         for(String s:keyList) sb.append(s).append("\n");
-        
         System.out.println(sb);
     }
 }
