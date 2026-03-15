1f9d9a732befa2141ad6adbd6ba239ce5d8457b9
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-16 08:45:05 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,14 +1,17 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
-    private static long fac(int n){
-        if(n>1) return n*fac(n-1);
-        else return 1;
+    
+    private static long fac(long n){
+        if(n==1 || n==0) return 1;
+        else return n*fac(n-1);
     }
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         System.out.println(fac(n));
+        
     }
 }
