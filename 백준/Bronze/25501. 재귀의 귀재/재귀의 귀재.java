592d19f9703475e0992d14f50860586fa8643a7e
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-16 08:56:29 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 224 ms, Memory: 19952 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -2,29 +2,34 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int[] recursion(char[]s, int start, int end, int cnt){
-        if(start>=end) return new int[]{1, cnt};
-        else if(s[start]!=s[end]) return new int[]{0, cnt};
-        else return recursion(s, start+1, end-1, cnt+1);
-    }
+    public static int cnt;
     
-    private static int[] isPalindrome(char[] s){
-        return recursion(s, 0, s.length-1, 1);
+    public static int recursion(String s, int l, int r, int c){
+        if(l>=r) {
+            cnt = c;
+            return 1;
+        }
+        else if (s.charAt(l) != s.charAt(r)) {
+            cnt = c;
+            return 0;
+        }
+        else return recursion(s, l+1, r-1, c+1);
     }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i<T; i++){
-            String str = br.readLine();
-            int[] result = isPalindrome(str.toCharArray());
-            sb.append(result[0]).append(" ").append(result[1]).append("\n");
+        int n = Integer.parseInt(br.readLine());
+        
+        
+        for(int i = 0; i<n; i++){
+            String word = br.readLine();
+            cnt = 0;
+            int k = recursion(word, 0, word.length()-1, 1);
+            sb.append(k + " " + cnt + "\n");
             
         }
-        
-        System.out.println(sb);
-        
+		System.out.println(sb);
     }
 }
