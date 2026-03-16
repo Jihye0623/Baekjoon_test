1aa1b354a4a19b2ea623a1bb75c8f7acb872c469
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-16 09:48:58 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16936 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,34 +1,34 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
-    private static int N, M;
+    private static int n, m;
     private static int[] arr;
     private static StringBuilder sb = new StringBuilder();
     
-    private static void backtrack(int start, int depth){
-        if(depth == M) {
-            for(int i:arr) sb.append(i).append(" ");
+    private static void backTrack(int idx, int depth){
+        if(depth==m) {
+            for(int a:arr) sb.append(a).append(" ");
             sb.append("\n");
             return;
         }
         
-        for(int i = start; i<=N; i++){
+        for(int i = idx; i<=n; i++){
             arr[depth] = i;
-            backtrack(i, depth+1);
+            backTrack(i, depth+1);
         }
     }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
         
-        backtrack(1, 0);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        backTrack(1, 0);
         
         System.out.println(sb);
-        
     }
 }
