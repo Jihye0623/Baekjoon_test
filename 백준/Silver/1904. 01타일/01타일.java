f105997ffb7c52e04bcc07bade0dcc394bad64ba
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-17 08:52:53 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 18168 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -2,22 +2,18 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int[] dp;
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        dp = new int[1000001];
+        
+        int[] dp = new int[1000001];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 3;
-        dp[4] = 5;
         
-        for(int i = 5; i<=n; i++){
-            dp[i]  = (dp[i-1]+dp[i-2])%15746;
+        for(int i = 4; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
         }
-        
-        System.out.println(dp[n]);
-        
+        System.out.println(dp[n] % 15746);
     }
 }
