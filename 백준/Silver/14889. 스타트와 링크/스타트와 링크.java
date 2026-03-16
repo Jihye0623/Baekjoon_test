181fa3f83dfd7db56c07585219159367f9a36690
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-16 11:08:24 +0900

    [Silver I] Title: 스타트와 링크, Time: 368 ms, Memory: 20476 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -2,48 +2,47 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int n, answer;
-    private static int[][] map;
+    private static int n, answer = Integer.MAX_VALUE;
+    private static int[][] arr;
     private static boolean[] visited;
     
-    private static void backtrack(int idx, int count){
-        if(count == n/2){
+    private static void backTrack(int idx, int depth){
+        if(depth==n/2){
             calculateDiff();
-            return ;
+            return;
         }
         
         for(int i = idx; i<n; i++){
             if(!visited[i]){
                 visited[i] = true;
-                backtrack(i+1, count+1);
+                backTrack(i+1, depth+1);
                 visited[i] = false;
             }
         }
     }
-
+    
     private static void calculateDiff(){
-        int startSum = 0;
-        int linkSum = 0;
-
+        int start = 0;
+        int link = 0;
+        
         for(int i = 0; i<n-1; i++){
             for(int j = i+1; j<n; j++){
                 if(visited[i] && visited[j]){
-                    startSum += map[i][j];
-                    startSum += map[j][i];
+                    start += arr[i][j];
+                    start += arr[j][i];
                 }
                 else if(!visited[i] && !visited[j]){
-                    linkSum += map[i][j];
-                    linkSum += map[j][i];
+                    link += arr[i][j];
+                    link += arr[j][i];
                 }
             }
         }
-
-        int val = Math.abs(startSum - linkSum);
-        if(val == 0){
+        
+        int val = Math.abs(start-link);
+        if(val==0){
             System.out.println(0);
             System.exit(0);
         }
-
         answer = Math.min(answer, val);
     }
     
@@ -51,18 +50,20 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        map = new int[n][n];
-        answer = Integer.MAX_VALUE;
+        
+        arr = new int[n][n];
         visited = new boolean[n];
         
         for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j<n; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());        
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
-		backtrack(0, 0);
+        backTrack(0,0);
+        
         System.out.println(answer);
+        
     }
 }
