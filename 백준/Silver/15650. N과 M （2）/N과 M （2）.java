a518038cbf62becedaf4f5494b40621ebceb1fc3
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-16 09:39:37 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    private static int n, m;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+    
+    private static void backTrack(int idx, int depth){
+        if(depth==m) {
+            for(int a:arr) sb.append(a).append(" ");
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i = idx; i<=n; i++){
+            arr[depth] = i;
+            backTrack(i+1, depth+1);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        backTrack(1, 0);
+        
+        System.out.println(sb);
+    }
+}
