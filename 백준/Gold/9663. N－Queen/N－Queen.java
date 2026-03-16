2633df5d05528c6a14f865608c6f50fed8fe5e89
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-16 10:22:43 +0900

    [Gold IV] Title: N-Queen, Time: 2640 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,35 +2,34 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int N;
-    private static int answer;
+    private static int N, answer;
     private static boolean[] width, q1, q2;
-    
-    private static void backtrack(int q){
-        if(q==N){
+    private static void backTrack(int row){
+        if(row == N) {
             answer++;
             return;
         }
         
-        for(int i = 1; i<=N; i++){
-            if(!(width[i]||q1[q+i]||q2[q-i+N])){
-                width[i] = q1[q+i] = q2[q-i+N] = true;
-                backtrack(q+1);
-                width[i] = q1[q+i] = q2[q-i+N] = false;
-                
+        for(int i = 0; i<N; i++){
+            if(!width[i] && !q1[row-i+N] && !q2[row+i]){
+                width[i] = q1[row-i+N] = q2[row+i] = true;
+                backTrack(row+1);
+                width[i] = q1[row-i+N] = q2[row+i] = false;
             }
         }
+        
     }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         answer = 0;
-        width = new boolean[N+1];
+        
+        width = new boolean[N];
         q1 = new boolean[N*2];
         q2 = new boolean[N*2];
         
-        backtrack(0);
+        backTrack(0);
         
         System.out.println(answer);
         
