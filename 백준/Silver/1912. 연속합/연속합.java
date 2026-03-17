e731dc24707d0047ec1d7d0085961be3d6c326ca
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-17 09:02:24 +0900

    [Silver II] Title: 연속합, Time: 228 ms, Memory: 24044 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,30 +2,25 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int[] dp;
-    private static int[] arr;
-    
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
         
-        arr = new int[n];
-        dp = new int[n];
-           
         StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
         for(int i = 0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        
+        int[] dp = new int[n];
         dp[0] = arr[0];
+        int answer = arr[0];
         for(int i = 1; i<n; i++){
-            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+            answer = Math.max(answer, dp[i]);
         }
         
-        int answer = Arrays.stream(dp).max().getAsInt();
         System.out.println(answer);
-        
-        
-        
     }
 }
