65b180a16744302e9a81d4d873cfad49b8cfd745
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-17 09:16:01 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 26468 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,34 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         
         int[][] arr = new int[n][n];
-        int[][] dp = new int[n][n];
-        
         for(int i = 0; i<n; i++){
-           st = new StringTokenizer(br.readLine());
-           for(int j = 0; j<i+1; j++){
-               arr[i][j] = Integer.parseInt(st.nextToken());
-           }
-        }
-        
-        for(int i = 0; i<n; i++){
-            dp[n-1][i] = arr[n-1][i];
-        }
-        
-        
-        for(int i = n-2; i>=0; i--){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j<i+1; j++){
-                dp[i][j] = Math.max(dp[i+1][j+1], dp[i+1][j]);
-                dp[i][j] += arr[i][j];
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
+        int[][] dp = new int[n][n];
+        for(int i = 0; i<n; i++)
+            dp[n-1][i] = arr[n-1][i];
+        
+        for(int i = n-2; i>=0; i--){
+            for(int j = 0; j<i+1; j++){
+                dp[i][j] = Math.max(dp[i+1][j], dp[i+1][j+1]) + arr[i][j];
+            }    
+        }
+        
         System.out.println(dp[0][0]);
+                
     }
 }
