799bd8fda38859c4d3a2e380c730a6f2c676e330
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-18 08:55:19 +0900

    [Silver III] Title: 수열, Time: 244 ms, Memory: 24264 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -6,28 +6,26 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
         
-        st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int answer = 0;
-        int value = 0;
-
-        for(int i = 0; i<m; i++){
-            value += arr[i];
+        
+        int answer =  0;
+        for(int i = 0; i<k; i++){
+            answer += arr[i];
         }
-        answer = value;
-
-        for(int i = 1; i<=n-m; i++){
-            value = value - arr[i-1] + arr[i+m-1];
-            answer = Math.max(value, answer);
+        
+        int now = answer;
+        for(int i = 1; i<=n-k; i++){
+            now = now - arr[i-1] + arr[i+k-1];
+            answer = Math.max(answer, now);
         }
-
+        
         System.out.println(answer);
+        
     }
 }
