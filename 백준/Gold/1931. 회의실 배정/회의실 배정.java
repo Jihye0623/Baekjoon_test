7ff165d4f91d71521b1488b239722efeef9d9460
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-19 08:53:41 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 43580 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -7,7 +7,7 @@ class Main{
         int n = Integer.parseInt(br.readLine());
         
         int[][] time = new int[n][2];
-        for(int i = 0; i<n; i++){
+        for(int i =0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             time[i][0] = Integer.parseInt(st.nextToken());
             time[i][1] = Integer.parseInt(st.nextToken());
@@ -15,25 +15,23 @@ class Main{
         
         Arrays.sort(time, new Comparator<int[]>(){
            @Override
-            public int compare(int[] o1, int[] o2){
-                if(o1[1] == o2[1]){
-                    return o1[0] - o2[0];
-                }
-                
-                return o1[1] - o2[1];
-            }
+           public int compare(int[] op1, int[] op2){
+               if(op1[1]==op2[1]) return op1[0] - op2[0];
+               return op1[1] - op2[1];
+           }
         });
         
-        int count = 0;
+        int answer = 0;
         int prev_end_time = 0;
         
-        for(int i=0; i<n; i++){
-            if(prev_end_time <= time[i][0]){
-                prev_end_time =  time[i][1];
-                count++;
+        for(int i = 0; i<n; i++){
+            if(prev_end_time<=time[i][0]){
+                prev_end_time = time[i][1];
+                answer++;
             }
+            
         }
         
-		System.out.println(count);
+        System.out.println(answer);
     }
 }
