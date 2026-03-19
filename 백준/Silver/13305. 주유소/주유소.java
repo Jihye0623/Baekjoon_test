e821f5d72746c273acf16ebb818561c4f692952e
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-19 09:17:47 +0900

    [Silver III] Title: 주유소, Time: 304 ms, Memory: 29928 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] road = new int[n-1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n-1; i++){
+            road[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] city = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            city[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int total_cost = city[0] * road[0];
+        int current_cost = city[0];
+        
+        for(int i = 1; i<n-1; i++){
+            current_cost = Math.min(current_cost, city[i]);
+            total_cost += current_cost * road[i];
+        }
+        System.out.println(total_cost);
+    }
+}
