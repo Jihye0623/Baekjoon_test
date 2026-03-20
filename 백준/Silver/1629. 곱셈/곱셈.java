922ed056caec15de75b49e03c1cf39503d711891
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-20 09:32:01 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,32 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+		    
         long a = Integer.parseInt(st.nextToken());
         long b = Integer.parseInt(st.nextToken());
         long c = Integer.parseInt(st.nextToken());
+
         long answer = 1;
-        
-        if(a>c) a = a%c;
-        
+        if(a>c) a %=c;
 
         while(true){
             if(b==1) break;
+
             if(b%2==1){
                 answer = (answer*a) % c;
                 b--;
             }
 
-            a = a*a % c;
+            a = a*a %c;
             b/=2;
         }
-
-        answer = answer * a %c;
+        answer = answer *a %c;
+        
 
         System.out.println(answer);
-
     }
 }
