5279d3f233f84ff11dd4c5ef6b7d8e1aedf10b7f
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-20 09:25:37 +0900

    [Silver II] Title: 종이의 개수, Time: 956 ms, Memory: 310948 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -2,55 +2,66 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int[][] arr;
-    private static int[] answer = new int[3];
+    private static int[][] map;
+    private static int num1 = 0, num2 = 0, num3 = 0;
     
-    private static boolean check(int row, int col, int size){
-        int c = arr[row][col];
+    private static void divide(int row, int col, int size){
+        
+        int num = map[row][col];
+        boolean flag = false;
+        
         for(int i = row; i<row+size; i++){
             for(int j = col; j<col+size; j++){
-                if(arr[i][j] != c) return false;
+                if(num!=map[i][j]){
+                    flag = true;
+                    break;
+                }
             }
         }
-        return true;
-    }
-    
-    private static void partition(int row, int col, int size){
-        if(check(row, col, size)){
-            answer[arr[row][col]+1] += 1;
+        
+        if(!flag){
+            if(num==-1) num1++;
+            else if(num==0) num2++;
+            else if(num==1) num3++;
             return;
         }
+        else{
+
+            int newSize = size/3;
+            divide(row, col, newSize);
+            
+            divide(row, col+newSize, newSize);
+            divide(row, col+newSize*2, newSize);
+            
+            divide(row+newSize, col, newSize);
+            divide(row+newSize*2, col, newSize);
+            
+            divide(row+newSize, col+newSize, newSize);   
+            divide(row+newSize, col+newSize*2, newSize);   
+            divide(row+newSize*2, col+newSize, newSize);  
+            divide(row+newSize*2, col+newSize*2, newSize);   
+        }
         
-        int newSize = size/3;
-        
-        partition(row, col, newSize);
-        partition(row, col+newSize, newSize);
-        partition(row, col+newSize*2, newSize);
-        
-        partition(row+newSize, col, newSize);
-        partition(row+newSize, col+newSize, newSize);
-        partition(row+newSize, col+newSize*2, newSize);
-        
-        partition(row+newSize*2, col, newSize);
-        partition(row+newSize*2, col+newSize, newSize);
-        partition(row+newSize*2, col+newSize*2, newSize);
     }
     
+    private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
+        
+        map = new int[n][n];
         
         for(int i = 0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j<n; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }    
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
+
+        divide(0,0,n);
         
-        partition(0,0,n);
-        
-        for(int i:answer) System.out.println(i);
-		
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
     }
 }
