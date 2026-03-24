b67a6230f19cd1341dc23327ce871b8d4911cf19
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-24 10:43:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17572 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -6,29 +6,33 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 		int k = Integer.parseInt(st.nextToken());
-		int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         
-        int[] arr = new int[k];
+        long[] len = new long[k];
         long max = 0;
         
         for(int i = 0; i<k; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            if(max<arr[i]) max = arr[i];
+            len[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, len[i]);
         }
+        
         
         max++;
         long min = 1;
         long mid = 0;
         
         while(min<max){
-            mid = (max+min)/2;
-            long count = 0;
+            mid = (max+min) / 2;
+            long sum = 0;
+            for(int i = 0; i<k; i++){
+                long t = len[i]/mid;
+                sum += t;
+            }
+            if(sum<n) max = mid;
+            else min = mid + 1;
             
-            for(int i = 0; i<arr.length; i++) count += arr[i]/mid;
-            
-            if(count<n) max = mid;
-            else min = mid+1;
         }
+        
         System.out.println(min-1);
     }
 }
