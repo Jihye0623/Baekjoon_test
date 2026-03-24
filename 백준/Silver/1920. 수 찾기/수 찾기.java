016a6a4f17d4af01c891280233f65eb928268e07
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-24 10:19:44 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 47472 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,39 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i = 0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
         
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<n; i++){
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i<m; i++){
-            int k = Integer.parseInt(st.nextToken());
-            int start = 0, end = n-1;
-            int flag = 0;
-            while(start<=end){
-                int mid = (start+end)/2;
-                
-                if(arr[mid]==k) {
-                    flag = 1;
-                    break;
-                }
-                else if (arr[mid]<k) start = mid+1;
-                else end = mid -1;
+            if(set.contains(Integer.parseInt(st.nextToken()))){
+                sb.append(1);
             }
-            
-            if(flag==0) sb.append(0).append("\n");
-            else sb.append(1).append("\n");
+            else{
+                sb.append(0);
+            }
+            sb.append("\n");
         }
-		
+        
         System.out.println(sb);
     }
 }
