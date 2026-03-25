8010801fea60d5bef68f82e1bdf5843a06128ea7
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-25 09:34:54 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 908 ms, Memory: 89940 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -2,52 +2,49 @@ import java.util.*;
 import java.io.*;
 
 class Main{
+    
     private static ArrayList<Integer>[] adjList;
     private static int[] visited;
-    private static int count = 1;
-    private static void dfs(int start){
-        visited[start] = count++;
-        
-        for(int i = 0; i<adjList[start].size(); i++){
-            int next = adjList[start].get(i);
-            if(visited[next]==0){
-                dfs(next);
-            }
+    private static int cnt = 1;
+    private static void dfs(int now){
+        visited[now] = cnt++;
+        for(int next:adjList[now]){
+            if(visited[next]==0) dfs(next);
         }
     }
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());    
-		int m = Integer.parseInt(st.nextToken());    
-		int k = Integer.parseInt(st.nextToken());  
+		
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
         
-        adjList = new ArrayList[n+1];
-        visited = new int[n+1];
+        adjList = new ArrayList[N+1];
+        visited = new int[N+1];
         
-        for(int i = 0; i<n+1; i++){
+        for(int i = 1; i<N+1; i++){
             adjList[i] = new ArrayList<>();
         }
         
-        for(int i = 0; i<m; i++){
+        for(int i = 0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             adjList[u].add(v);
             adjList[v].add(u);
         }
-        
-         for(int i = 0; i<n+1; i++){
+
+        for(int i = 1; i<N+1; i++){
             Collections.sort(adjList[i]);
         }
         
-        dfs(k);
+        dfs(R);
         
         StringBuilder sb = new StringBuilder();
-        
-        for(int i = 1; i<=n; i++) sb.append(visited[i]).append("\n");
+        for(int i = 1; i<N+1; i++) sb.append(visited[i]).append("\n");
         System.out.println(sb);
-        
         
     }
 }
