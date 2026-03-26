04843eadc0459ae8182fce6982074f21f74e5773
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-26 18:30:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 23376 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+class Main{    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        if(N==K){
+            System.out.println(0);
+            return;
+        }
+        
+        boolean[] visited = new boolean[100001];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{N,0});
+        visited[N] = true;
+        
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int pos = current[0];
+            int time = current[1];
+            
+            if(pos==K){
+                System.out.println(time);
+                return;
+            }
+            
+            int[] nextPositions = {pos-1, pos+1, pos*2};
+            
+            for(int next:nextPositions){
+                if(next>=0 && next<=100000 && !visited[next]){
+                    visited[next] = true;
+                    queue.add(new int[]{next, time+1});
+                }
+            }
+        }
+    }
+}
