dc2bee0d0b2bfe73e55e8c47bb7d43b28f5c5ca8
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-03-26 16:48:50 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 
 class Main{
+    private static int N, M;
     private static int[][] map;
-    private static int n, m;
     private static int[] dx = {0,0,-1,1};
     private static int[] dy = {-1,1,0,0};
     
@@ -21,13 +21,16 @@ class Main{
         
         while(!queue.isEmpty()){
             Node now = queue.pollFirst();
+            
             for(int i = 0; i<4; i++){
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
-                if(nx<0 || ny<0 || nx>=n||ny>=m) continue;
-                if(map[nx][ny]==0 || map[nx][ny]>1) continue;
-                map[nx][ny] = map[now.x][now.y] + 1;
-                queue.addLast(new Node(nx,ny));
+                
+                if(nx<0 || ny<0 || nx>=N || ny >= M) continue;
+                if(map[nx][ny]!=1) continue;
+                
+                map[nx][ny] = map[now.x][now.y] +1;
+                queue.addLast(new Node(nx, ny));
             }
         }
     }
@@ -35,18 +38,20 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+		
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
         
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<N; i++){
             String str = br.readLine();
-            for(int j = 0; j<m; j++){
-                map[i][j] = str.charAt(j) - '0';
+            for(int j = 0; j<M; j++){
+                map[i][j] = (int)(str.charAt(j) - '0');
             }
         }
         
         bfs(new Node(0,0));
-        System.out.println(map[n-1][m-1]);
+        
+        System.out.println(map[N-1][M-1]);
     }
 }
