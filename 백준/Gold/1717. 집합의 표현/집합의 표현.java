b827e0fdb48523b03510fdc10ed5b56a1cec4aaf
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-02 18:29:36 +0900

    [Gold V] Title: 집합의 표현, Time: 412 ms, Memory: 51976 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,45 +3,49 @@ import java.io.*;
 
 class Main{
     private static int[] parent;
-    private static int find(int a){
-        if(a==parent[a]) return a;
-        else return parent[a] = find(parent[a]);
+    
+    private static int find(int x){
+        if(x==parent[x]) return x;
+        else return parent[x] = find(parent[x]);
     }
     
     private static void union(int a, int b){
-        int parentA = find(a);
-        int parentB = find(b);
-        parent[parentA] = parentB;
+        int pa = find(a);
+        int pb = find(b);
+        
+        parent[pa] = pb;
     }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-        parent = new int[n+1];
+		
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         
-        for(int i = 1; i<=n; i++){
+        parent = new int[n+1];
+        for(int i = 0; i<=n; i++){
             parent[i] = i;
         }
         
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-		    int op = Integer.parseInt(st.nextToken());
-		    int a = Integer.parseInt(st.nextToken());
-		    int b = Integer.parseInt(st.nextToken());
-            if(op==0){
-                union(a,b);
-            } 
+		
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if(op==0) union(a,b);
             else{
-                if(find(a)!=find(b)) sb.append("NO");
-                else sb.append("YES");
-                sb.append("\n");
+                if(find(a)!=find(b)) sb.append("NO\n");
+                else sb.append("YES\n");
             }
+        
         }
         
         System.out.println(sb);
+        
         
     }
 }
