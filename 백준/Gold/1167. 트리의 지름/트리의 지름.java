dfb8285167b434825e64355f91344f6c4f74681b
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-02 18:30:21 +0900

    [Gold II] Title: 트리의 지름, Time: 676 ms, Memory: 96000 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -2,64 +2,62 @@ import java.io.*;
 import java.util.*;
 
 class Main{
+    private static ArrayList<ArrayList<Node>> graph;
     private static int result = 0, depart = 1;
     private static boolean[] visited;
-    private static ArrayList<ArrayList<Node>> graph;
     
-    public static class Node{
-        int num, cost;
+    private static class Node{
+        int n, cost;
         
-        public Node(int num, int cost){
-            this.num = num;
+        public Node(int n, int cost){
+            this.n = n;
             this.cost = cost;
         }
     }
     
     private static void dfs(int now, int sum){
         visited[now] = true;
-        if(result < sum){
+        if(sum>result){
             result = sum;
             depart = now;
         }
         
         for(Node next:graph.get(now)){
-            if(!visited[next.num]){
-                dfs(next.num, sum + next.cost);
+            if(!visited[next.n]){
+                dfs(next.n, next.cost + sum);
             }
         }
     }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int v = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         
         graph = new ArrayList<>();
-        for(int i = 0; i<=v; i++){
+        for(int i = 0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
         
-        for(int i = 0; i<v; i++){
+        for(int i = 0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            
             int num = Integer.parseInt(st.nextToken());
             
             while(true){
-                int k = Integer.parseInt(st.nextToken());
-                if(k==-1) break;
+                int node = Integer.parseInt(st.nextToken());
+                if(node == -1) break;
                 
                 int cost = Integer.parseInt(st.nextToken());
-                graph.get(num).add(new Node(k, cost));
+                graph.get(num).add(new Node(node, cost));
             }
         }
         
-        visited = new boolean[v+1];
+        visited = new boolean[n+1];
         dfs(1,0);
         
-        visited = new boolean[v+1];
+        visited = new boolean[n+1];
         dfs(depart,0);
         
         System.out.println(result);
-        
         
     }
 }
