8f17a2eac33dc4dd1d8fd7a0f6bcb32c22a41496
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-03 15:43:18 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 572 ms, Memory: 68564 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,72 +2,80 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static int N, E;
     private static ArrayList<ArrayList<Node>> graph;
+    private static int n;    
+    
     private static class Node{
-        int num,cost;
+        int num, cost;
+        
         public Node(int num, int cost){
             this.num = num;
             this.cost = cost;
         }
     }
     
-    private static int[] dijkstra(int start){
-        int[] dist = new int[N+1];
-        for(int i = 1; i<=N; i++) dist[i] = 200000000;
-        dist[start] = 0;
+    private static int[] d(int start){
+        int[] distance = new int[n+1];
+        for(int i = 0; i<=n; i++) distance[i] = 200000000;
+        distance[start] = 0;
         
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2)->Integer.compare(o1.cost, o2.cost));
-        queue.add(new Node(start,0));
+        PriorityQueue<Node> queue = new PriorityQueue<>(new Comparator<Node>(){
+           @Override
+            public int compare(Node n1, Node n2){
+                return Integer.compare(n1.cost, n2.cost);
+            }
+        });
+        
+        queue.add(new Node(start, 0));
         
         while(!queue.isEmpty()){
             Node now = queue.poll();
-            if(now.cost > dist[now.num]) continue;
             
-            for(Node next : graph.get(now.num)){
-                if(dist[next.num]>now.cost + next.cost){
-                    dist[next.num] = now.cost + next.cost;
-                    queue.add(new Node(next.num, dist[next.num]));
+            if(now.cost > distance[now.num]) continue;
+            
+            for(Node next:graph.get(now.num)){
+                if(distance[next.num]> now.cost + next.cost){
+                    distance[next.num] = now.cost + next.cost;
+                    queue.add(new Node(next.num, distance[next.num]));
                 }
             }
         }
         
-        return dist;
+        return distance;
     }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());    
-		E = Integer.parseInt(st.nextToken());    
-        
+		
+        n = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
         graph = new ArrayList<>();
-        for(int i = 0; i<=N; i++){
-            graph.add(new ArrayList<>());
-        }
         
-        for(int i = 0; i<E; i++){
+        for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
+        
+        for(int i = 0; i<e; i++){
             st = new StringTokenizer(br.readLine());
-		    int a = Integer.parseInt(st.nextToken());    
-		    int b = Integer.parseInt(st.nextToken());   
-		    int c = Integer.parseInt(st.nextToken()); 
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
             graph.get(a).add(new Node(b,c));
             graph.get(b).add(new Node(a,c));
         }
+        
+        
         st = new StringTokenizer(br.readLine());
-		int v1 = Integer.parseInt(st.nextToken());    
-		int v2 = Integer.parseInt(st.nextToken());  
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
         
-        int[] distFrom1 = dijkstra(1);
-        int[] distFromV1 = dijkstra(v1);
-        int[] distFromV2 = dijkstra(v2);
+        int[] from = d(1);
+        int[] fromV1 = d(v1);
+        int[] fromV2 = d(v2);
         
-        int path1 = distFrom1[v1] + distFromV1[v2] + distFromV2[N];
-        int path2 = distFrom1[v2] + distFromV2[v1] + distFromV1[N];
-        int answer = Math.min(path1, path2);
-        
-        if(answer>=200000000) System.out.println(-1);
-        else System.out.println(answer);
-        
+        int path1 = from[v1] + fromV1[v2] + fromV2[n];
+        int path2 = from[v2] + fromV2[v1] + fromV1[n];
+
+        if(path1>=200000000 && path2>=200000000) System.out.println(-1);
+        else System.out.println(Math.min(path1, path2));
     }
 }
