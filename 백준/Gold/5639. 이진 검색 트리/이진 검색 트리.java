9413fa198c705da5c53f135fd5d24f79da2dc173
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-03 16:30:55 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 488 ms, Memory: 18040 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -2,31 +2,31 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    private static StringBuilder sb = new StringBuilder();
-    
+    static StringBuilder sb = new StringBuilder();
+
     private static class Node{
         int num;
         Node left, right;
-
+        
         public Node(int num){
             this.num = num;
         }
-
-        public void insert(int n){
-            if(n<this.num){
-                if(this.left == null) this.left = new Node(n);
-                else this.left.insert(n);
+        
+        public void insert(int num){
+            if(this.num>num) {
+                if(this.left==null) this.left = new Node(num);
+                else this.left.insert(num);
             }
-            else{
-                if(this.right == null) this.right = new Node(n);
-                else this.right.insert(n);
+            else {
+                if(this.right==null) this.right = new Node(num);
+                else this.right.insert(num);
             }
         }
     }
-
+    
     private static void postorder(Node node){
-        if(node == null) return;
-
+        if(node==null) return;
+        
         postorder(node.left);
         postorder(node.right);
         sb.append(node.num).append("\n");
@@ -34,14 +34,14 @@ class Main{
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Node root = new Node(Integer.parseInt(br.readLine()));
-        String input = "";
         
-        while((input=br.readLine()) !=null && !input.isEmpty()){
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        
+        String input = "";
+        while((input=br.readLine())!=null && !input.isEmpty()){
             root.insert(Integer.parseInt(input));
         }
-		    
+        
         postorder(root);
         System.out.println(sb);
     }
