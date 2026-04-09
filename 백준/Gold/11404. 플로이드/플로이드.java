b311e4788828ac49b38bbdc144924086b1e4f515
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-09 09:42:00 +0900

    [Gold IV] Title: 플로이드, Time: 368 ms, Memory: 43332 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static final int INF = 20000000;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        
+        int[][] dist = new int[n+1][n+1];
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                dist[i][j] = INF;
+            }
+        }
+        for(int i = 1; i<=n; i++){
+            dist[i][i] = 0;
+        }
+        
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+        
+        for(int k = 1; k<=n; k++){
+            for(int i = 1; i<=n; i++){
+                for(int j = 1; j<=n; j++){
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        
+		StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                int d = dist[i][j];
+                if(d>=INF) sb.append(0).append(" ");
+                else sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
