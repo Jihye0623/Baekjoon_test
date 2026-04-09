d04fe6cb3e5b1ab8d8a0f2cbb2dcb13923b3be6d
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-09 09:20:20 +0900

    [Gold IV] Title: 타임머신, Time: 284 ms, Memory: 19316 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static class Edge{
+    private static class Edge{
         int from, to, cost;
         public Edge(int from, int to, int cost){
             this.from = from;
@@ -10,55 +10,55 @@ class Main{
             this.cost = cost;
         }
     }
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+        
+        int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        ArrayList<Edge> edges = new ArrayList<>();
+        ArrayList<Edge> list = new ArrayList<>();
         for(int i = 0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-	    	int a = Integer.parseInt(st.nextToken());
+        
+            int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(a,b,c));
+            
+            list.add(new Edge(a,b,c));
         }
         
         long[] dist = new long[n+1];
         Arrays.fill(dist, Long.MAX_VALUE);
         dist[1] = 0;
         
-        for(int i = 1; i<n; i++){
-            for(Edge edge:edges){
-                if(dist[edge.from]!= Long.MAX_VALUE && dist[edge.to] > dist[edge.from] + edge.cost){
+        for(int i=1; i<n; i++){
+            for(Edge edge: list){
+                if(dist[edge.from]!=Long.MAX_VALUE && dist[edge.to]>dist[edge.from]+edge.cost){
                     dist[edge.to] = dist[edge.from] + edge.cost;
                 }
             }
         }
         
-        boolean hasNegativeCycle = false;
-        for(Edge edge:edges){
-            if(dist[edge.from] != Long.MAX_VALUE && dist[edge.to] > dist[edge.from] + edge.cost){
-                hasNegativeCycle = true;
-                break;
+        boolean isMinus = false;
+        for(int i=1; i<n; i++){
+            for(Edge edge: list){
+                if(dist[edge.from]!=Long.MAX_VALUE && dist[edge.to]>dist[edge.from]+edge.cost){
+                    isMinus = true;
+                    break;
+                }
             }
         }
         
-        if(hasNegativeCycle) System.out.println(-1);
-        else{    
-            StringBuilder sb = new StringBuilder();   
-            for(int i = 2; i<=n; i++){
-                if(dist[i] == Long.MAX_VALUE){
-                    sb.append("-1\n");
-                }
-                else{
-                    sb.append(dist[i]).append("\n");
-                }
+        if(isMinus) System.out.println(-1);
+        else{
+            StringBuilder sb = new StringBuilder();
+            for(int i=2; i<=n; i++) {
+                if(dist[i]!=Long.MAX_VALUE) sb.append(dist[i]).append("\n");
+                else sb.append(-1).append("\n");
             }
-            
             System.out.println(sb);
         }
+        
     }
 }
