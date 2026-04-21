499e917d81e53265ebff9b8834b3d0f3aeabeb7a
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-04-21 10:50:38 +0900

    [level 2] Title: [1차] 캐시, Time: 31.33 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if(cacheSize==0) return cities.length * 5;
+        
+        int answer = 0;
+        LinkedList<String> cache = new LinkedList<>();
+        
+        for(String city:cities){
+            city = city.toLowerCase();
+            
+            if(cache.contains(city)){
+                cache.remove(city);
+                cache.add(city);
+                answer+=1;
+            }
+            else{
+                if(cache.size()>=cacheSize){
+                    cache.remove(0);
+                }
+                cache.add(city);
+                answer += 5;
+            }
+        }
+        
+        return answer;
+    }
+}
