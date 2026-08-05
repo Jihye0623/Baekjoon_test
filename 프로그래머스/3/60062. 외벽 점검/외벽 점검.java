682b4e3ba52ec4832abcb2a6caf72c5117820f95
Jihye <123048615+Jihye0623@users.noreply.github.com>
2026-08-05 17:14:32 +0900

    [level 3] Title: 외벽 점검, Time: 22.49 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     private static int length, answer;
@@ -8,23 +8,24 @@ class Solution {
     private static boolean check(int[] dist){
         for(int i = 0; i<length; i++){
             int idx = i;
-            for(int distance : dist){
+            for(int distance:dist){
                 int position = Weak[idx++] + distance;
-                while(idx<Weak.length && Weak[idx] <= position)
+                while(idx < Weak.length && Weak[idx] <= position){
                     idx++;
+                }
             }
             
-        if(idx-i>=length)
-            return true;
+            if(idx-i>=length) return true;
         }
         return false;
     }
     
     private static void backtrack(int n, int[] dist, int[] org){
         if(n==org.length){
-            if(check(dist))
+            if(check(dist)){
                 answer = n;
-            return;
+                return;
+            }
         }
         
         for(int i = 0; i<org.length; i++){
@@ -39,12 +40,13 @@ class Solution {
     
     public int solution(int n, int[] weak, int[] dist) {
         length = weak.length;
-        Weak = new int[length*2];
-        for(int i =0; i<2; i++){
-            for(int j =0; j<length; j++){
-                Weak[j+(i*length)] = weak[j] + (i*n);
+        Weak = new int[length * 2];
+        for(int i = 0; i<2; i++){
+            for(int j = 0; j<length; j++){
+                Weak[j + (i*length)] = weak[j] + (i*n);
             }
         }
+        
         Arrays.sort(dist);
         answer = -1;
         used = new boolean[dist.length];
@@ -52,9 +54,10 @@ class Solution {
         for(int i = 1; i<=dist.length; i++){
             int[] org = new int[i];
             System.arraycopy(dist, dist.length-i, org, 0, i);
-            backtrack(0,new int[i], org);
+            backtrack(0, new int[i], org);
             if(answer>0) break;
         }
+        
         return answer;
     }
 }
